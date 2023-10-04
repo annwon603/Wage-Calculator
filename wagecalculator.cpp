e8c986b33f34d4021f99139d7b5d3d664a511911
annwon603 <101071975+annwon603@users.noreply.github.com>
2023-10-03 21:10:43 -0700

    Revised my calculator
    1. Input your hourly rate
    2. Have an option to do the process all over again
    3. Added a boolean

--- wagecalculator.cpp
+++ wagecalculator.cpp
@@ -3,30 +3,46 @@
 using namespace std;
 int main()
 {
-    const int rate=16;
+    int rate;
     double hours1;
     double hours2;
     double wage;
     double total;
     double tax;
+    bool again=true;
+    string choice;
+    while(again){
+        cout<<"Enter your hourly rate: ";
+        cin>>rate;
+        cout<<"Enter your hours for first week: ";
+        cin>>hours1;
+        cout<<endl;
+        cout<<"Enter your hours for second week: ";
+        cin>>hours2;
+        cout<<endl;
+        cout<<"You worked for "<<hours1+hours2<<" in total"<<endl;
+        wage=rate*(hours1+hours2);
+        cout<<"Gross income: "<<wage<<endl;
+        cout<<"Enter tax percentage between .75 and .95: "<<endl;
+        tax= .75<=.95;
+        cin>>tax;
+        total=wage*tax;
+        cout<<"Tax income: "<<total<<"\n"<<endl;
+        cout<<"Again?"<<endl;
+        cout<<"Type in yes or no"<<"\n";
 
-    cout<<"Enter your hours for first week: ";
-    cin>>hours1;
-    cout<<endl;
-    cout<<"Enter your hours for second week: ";
-    cin>>hours2;
-    cout<<endl;
-    cout<<"You worked for "<<hours1+hours2<<" in total"<<endl;
-    wage=rate*(hours1+hours2);
-    cout<<"Gross income: "<<wage<<endl;
-    cout<<"Enter tax percentage between .75 and .95: "<<endl;
-    tax= .75<=.95;
-    cin>>tax;
-    total=wage*tax;
-    cout<<"Tax income: "<<total<<"\n"<<endl;
-    cout<<"Again?"<<endl;
-
+        cin>>choice;
+        if(choice=="yes"){
+            again=true;
+        }
+        else if(choice=="no"){
+            again=false;
+        }
+        else{
+            cout<<"Invalid statement, please type again";
+            cin>>choice;
+        }
+    }
 
     return 0;
 }
-
